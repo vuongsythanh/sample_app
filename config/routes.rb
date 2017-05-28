49d17dec4150faeb6017a089b88aa871e99a3af2
vuongsythanh <vuong.sy.thanh@framgia.com>
2017-05-28 19:07:52 +0700

    create static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root "application#hello"
   get "/hello", to: "application#hello"
+  get "/home", to: "static_pages#home"
+  get "/help", to: "static_pages#help"
+
 end
